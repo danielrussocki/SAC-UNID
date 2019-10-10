c30c016c19709d1f94c765e1294c038e5654e7d9
AlanSalas <dubekone@gmail.com>
2019-10-10 14:59:46 -0500

    *Update Login - Insertar nuevo usuario - Enviar correo - Ingresar con enter

--- modulos/login/consultas.php
+++ modulos/login/consultas.php
@@ -7,6 +7,10 @@
 				login();
 				break;
 			
+			case 'register':
+				register();
+				break;
+				
 			default:
 				break;
 		}
@@ -16,14 +20,15 @@
 		global $db;
 		$respuesta = [];
 
-		if ($_POST['usuario'] !=  "") {
+		if ($_POST['usuario'] !=  "") {	
 
-			$usuario = $db->get("usuarios", "*", ["email_usr" => $_POST['usuario']]);;
+			$usuario = $db->get("usuarios", "*", ["email_usr" => $_POST['usuario']]);
 
 			if ($usuario) {
-				
-				if ($usuario = $db->get("usuarios", "*", ["AND" => ["email_usr" => $_POST['usuario'], "password_usr"=> $_POST['password']]])) {
 
+				if ($usuario = $db->get("usuarios", "*", ["AND" => ["email_usr" => $_POST['usuario'], "status_usr"=> 0]])) {
+					$respuesta["status"] = 6;
+				}elseif ($usuario = $db->get("usuarios", "*", ["AND" => ["email_usr" => $_POST['usuario'], "password_usr"=> $_POST['password']]])) {
 					session_start();
 					error_reporting(0);
 					$_SESSION['id'] = $usuario["id_usr"];
@@ -36,12 +41,68 @@
 				}else{
 					$respuesta["status"] = 2;
 				}
-			}else {
+
+			}else{
 				$respuesta["status"] = 4;
 			}
-	}else{
-		$respuesta["status"] = 5;
+		
+		}else{
+			$respuesta["status"] = 5;
+		}
+		
+		echo json_encode($respuesta);
 	}
-	echo json_encode($respuesta);
-}
+
+	function register(){
+		global $db;
+		$respuesta = [];
+		$duplicate = false;
+		
+			if ($_POST["contraseña"] != $_POST["contraseña2"]) {
+				$respuesta["status"] = 3;
+			}elseif ($_POST["matricula"]  != ""  && $_POST["nombre"]  != ""  && $_POST["telefono"]  != ""  && $_POST["email"]  != "" &&  $_POST["contraseña"]  != "" &&  $_POST["contraseña2"]  != "") {
+
+            	$matriculas = $db->select("usuarios","matricula_usr");
+            		foreach ($matriculas as $matricula) {
+
+                		if($matricula == $_POST["matricula"]){
+                    		$duplicate = true;
+						}
+						
+					} 
+					
+					if (!$duplicate) {
+                		$usuarios = $db->insert('usuarios',[
+                    	"matricula_usr" => $_POST["matricula"],
+                    	"nombre_usr" => $_POST["nombre"],
+                    	"telefono_usr" => $_POST["telefono"],
+                    	"email_usr" => $_POST["email"],
+                    	"nivel_usr" => "1",
+                    	"status_usr" => "0",
+                    	"password_usr" => $_POST["contraseña"]
+						]);
+						 
+                    		if ($usuarios) {
+								//Envio de correo
+								$to = $_POST["email"];
+								$subject = 'Activación de correo';
+								$msg = '<h1>Hola, bienvenido a nuestro sistema de apartado de cañones.</h1> <p>Link de activación: ...</p>';
+								$headers = "From: Sistema Cañones <sistemacanones@smoothoperators.com.mx>\r\n";
+								$headers = "Reply To: replyto@smoothoperators.com.mx\r\n";
+								$headers .= "Content-type text/html\r\n";
+								mail($to, $subject, $msg, $headers);
+                            	$respuesta["status"] = 1;
+                    		} else {
+                        		$respuesta["status"] = 0;
+                    	}
+
+                	} else {
+                    	$respuesta["status"] = 2;
+					}
+				  
+    		} else {
+        		$respuesta["status"] = 0;
+			}
+		echo json_encode($respuesta);
+	}
 ?>
--- modulos/login/main.js
+++ modulos/login/main.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
   var remember = $.cookie("remember");
   if (remember == "true") {
     var username = $.cookie("username");
@@ -7,13 +8,14 @@ $(document).ready(function() {
     $("#inputPassword").attr("value", password);
     $("#inputRemember").prop("checked", remember);
   }
-  $("#btn-login").click(function() {
+
+  function processLogin() {
     let obj = {
       accion: "login",
     };
     $("#login-form")
       .find("input")
-      .map(function(i, e) {
+      .map(function (i, e) {
         //Añanir atributo
         obj[$(this).prop("name")] = $(this).val();
         if ($(this).prop("type") == "checkbox") {
@@ -21,9 +23,15 @@ $(document).ready(function() {
         }
       });
     if (obj.recordar == true) {
-      $.cookie("username", obj.usuario, { expires: 30 });
-      $.cookie("password", obj.password, { expires: 30 });
-      $.cookie("remember", true, { expires: 30 });
+      $.cookie("username", obj.usuario, {
+        expires: 30
+      });
+      $.cookie("password", obj.password, {
+        expires: 30
+      });
+      $.cookie("remember", true, {
+        expires: 30
+      });
     } else {
       $.cookie("username", null);
       $.cookie("password", null);
@@ -32,21 +40,9 @@ $(document).ready(function() {
     $.post(
       "/modulos/login/consultas.php",
       obj,
-      function(respuesta) {
+      function (respuesta) {
         if (respuesta.status == 3) {
-          swal({
-            title: "Datos Correctos",
-            text: "¿Deseas iniciar sesión?",
-            icon: "success",
-            buttons: true,
-            ConfirmButtonText: true,
-          }).then(willLogin => {
-            if (willLogin) {
-              window.location.href = "/index.php";
-            } else {
-              location.reload();
-            }
-          });
+          window.location.href = "/index.php";
         }
         if (respuesta.status == 5) {
           swal("¡ERROR!", "Campos vacios", "error");
@@ -57,14 +53,62 @@ $(document).ready(function() {
         if (respuesta.status == 4) {
           swal("¡ERROR!", "Usuario no registrado", "error");
         }
+        if (respuesta.status == 6) {
+          swal("¡CUENTA SIN ACTIVAR!", "Se ha enviado un email a su dirección de correo electronico para activar su cuenta", "error");
+        }
       },
       "JSON"
     );
+  }
+
+  function processRegister() {
+    let obj = {
+      accion: "register"
+    }
+    $("#register-form").find("input").map(function (i, e) {
+      obj[$(this).prop("name")] = $(this).val();
+    });
+    $.post("/modulos/login/consultas.php", obj, function (respuesta) {
+      if (respuesta.status == 0) {
+        swal("¡ERROR!", "Campos vacios", "error");
+      } else if (respuesta.status == 1) {
+        swal("Éxito", "Usuario insertado correctamente, le hemos envíado un email a su correo para activar su cuenta", "success").then(
+          () => {
+            location.reload();
+          }
+        );
+      } else if (respuesta.status == 2) {
+        swal("¡ERROR!", "La matricula ya existe", "error");
+      } else if (respuesta.status == 3) {
+        swal("¡ERROR!", "Las contraseñas no coinciden", "error");
+      } else {
+        errorAlert();
+      }
+    }, "JSON");
+  }
+
+  $("#btn-login").click(function () {
+    processLogin();
   });
 
-  $(".message a").click(function() {
-    $("form").animate(
-      {
+  $("#inputEmail, #inputPassword, #inputRemember").keyup(function (e) {
+    if (e.keyCode == 13) {
+      processLogin();
+    }
+  });
+
+  $("#btn-register").click(function () {
+    processRegister();
+  });
+
+  $("#matricula, #nombre, #telefono, #email, #contraseña, #contraseña2").keyup(function (e) {
+    if (e.keyCode == 13) {
+      processRegister();
+    }
+  });
+
+  $(".message a").click(function () {
+    $("form").animate({
         height: "toggle",
         opacity: "toggle",
       },
