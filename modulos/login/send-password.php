cebd2c2873734372a6a4f7660ed6e51cc442a343
MarioDMC <MarioDMC@users.noreply.github.com>
2019-10-20 21:26:13 -0500

    Update Email Config

--- modulos/login/send-password.php
+++ modulos/login/send-password.php
@@ -33,21 +33,15 @@ require $_SERVER["DOCUMENT_ROOT"] . 'vendor/autoload.php';
                 $addKey = substr(md5(uniqid(rand(), 1)), 3, 10);
                 $key = $key . $addKey;
                 // Insert Temp Table
-                $db->insert("password_reset_temp", [
-                "email" => $email,
-                "key" => $key,
-                "expDate" => $expDate
-            ]);
                 $message='<p>Querido usuario</p>';
                 $message = '<p>Hemos recibido una petición para un cambio de contraseña. 
-                Aquí encontrara el link para hacer el cambio: <br>';
-                $message .='<a href="' . $url . '">'. $url .'</a></p>';
+                Aquí encontrara el link para hacer el cambio: </p><br>';
                 $message .='<p><a href="http://smoothoperators.com.mx/modulos/login/create-new-password.php?key='.$key.'&email='.$email.'&action=reset" target="_blank">
-                 http://www.smoothoperators.com.mx/modulos/login/reset-request.php?key='.$key.'&email='.$email.'&action=reset</a></p>';
+                 http://www.smoothoperators.com.mx/modulos/login/create-new-password.php?key='.$key.'&email='.$email.'&action=reset</a></p>';
                 $body = $message;
                 $subject = "Restaurar contraseña";
                 $email_to = $email;
-                $fromserver = "sistemacanones@smoothoperators.com.mx";
+                $fromserver = "mail@smoothoperators.com.mx";
                 $from_name = "Sistema Cañones";
                 $mail = new PHPMailer();
                 $mail->CharSet = "utf-8";
@@ -57,9 +51,9 @@ require $_SERVER["DOCUMENT_ROOT"] . 'vendor/autoload.php';
                 $mail->IsSMTP();
                 $mail->SMTPAuth = true;
                 $mail->SMTPSecure = 'ssl';
-                $mail->Username = 'sistemacanones@smoothoperators.com.mx';
-                $mail->Password = '&7yQ=b&<';
-                $mail->From = "sistemacanones@smoothoperators.com.mx";
+                $mail->Username = 'mail.smoothoperators@gmail.com';
+                $mail->Password = 'Goodluck13';
+                $mail->From = "mail@smoothoperators.com.mx";
                 $mail->FromName = "Sistema Cañones";
                 $mail->Sender = $fromserver;
                 $mail->Subject = $subject;
@@ -67,9 +61,15 @@ require $_SERVER["DOCUMENT_ROOT"] . 'vendor/autoload.php';
                 $mail->IsHTML(true);
                 $mail->AddAddress($email_to);
                 if (!$mail->Send()) {
-                    header("HTTP/1.0 404 Not Found");
+                   header("HTTP/1.0 404 Not Found");
+                   //print_r("Message could not be sent 1. Mailer Error: {$mail->ErrorInfo}");
                 }
                 else {
+                    $db->insert("password_reset_temp", [
+                        "email" => $email,
+                        "key" => $key,
+                        "expDate" => $expDate
+                    ]);
                     header("Location: /modulos/login/reset-password.php?reset=success");
                 }
             }
